62e44c2c4eeb8165cfc90043d624f02a943eb8aa
Andrea Venuta <venutawebdesign@gmail.com>
2023-07-01 15:26:14 +0200

    Added tests and changed grammar

--- src/parser.rs
+++ src/parser.rs
@@ -7,8 +7,72 @@ pub struct WhispParser;
 
 #[cfg(test)]
 mod tests {
+    use pest::iterators::Pairs;
+
     use super::*;
 
+    const SAMPLE1: &str = include_str!("../examples/git_aliases.whisp");
+
+    fn print_tree(pairs: Pairs<'_, Rule>, indent: usize) {
+        for pair in pairs {
+            let rule = format!("{:?}", pair.as_rule());
+            let text = pair.as_str();
+            for _ in 0..indent {
+                print!("  ");
+            }
+            print!("\x1b[34;1m{rule}\x1b[0m");
+            for _ in 0..(32 - indent * 2 - rule.len()) {
+                print!(" ");
+            }
+            println!("{text}");
+            print_tree(pair.into_inner(), indent + 1);
+        }
+    }
+
+    fn parse(rule: Rule, code: &str) {
+        match WhispParser::parse(rule, code.trim()) {
+            Ok(pairs) => print_tree(pairs, 0),
+            Err(e) => {
+                eprintln!("{e}");
+                panic!();
+            }
+        }
+    }
+
     #[test]
-    fn test_load_grammar() {}
+    fn test_string() {
+        parse(Rule::string, r#""string""#);
+        parse(Rule::string, r#""another string""#);
+        parse(Rule::string, r#"another_string"#);
+        parse(Rule::string, r#"another_string!_no,-seriously"#);
+    }
+
+    #[test]
+    fn test_statement_block() {
+        // Single terminal function call statements on a single line
+        parse(Rule::statement_block, r#"{cur_branch}"#);
+        parse(Rule::statement_block, r#"{ cur_branch }"#);
+        parse(Rule::statement_block, r#"{ run git checkout main }"#);
+        // Non-terminal function call statement on a single line
+        parse(Rule::statement_block, r#"{ run git checkout main; }"#);
+        // Function call statement on multiple lines
+        parse(
+            Rule::statement_block,
+            r#"{ 
+              cur_branch 
+            }"#,
+        );
+        parse(
+            Rule::statement_block,
+            r#"{ 
+              cur_branch 
+            }"#,
+        );
+    }
+
+    // #[test]
+    // fn test_load_grammar() {
+    //     let res = WhispParser::parse(Rule::program, SAMPLE1);
+    //     println!("{res:#?}");
+    // }
 }
