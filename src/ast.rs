a48c245e200da5d65341c5c34b8dc130045811de
Andrea Venuta <venutawebdesign@gmail.com>
2023-07-04 17:06:28 +0200

    Small changes

--- src/ast.rs
+++ src/ast.rs
@@ -21,10 +21,12 @@ impl From<pest::error::Error<Rule>> for Error {
     }
 }
 
+/// Language string type.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WhispString(String);
 
 impl WhispString {
+    /// Create a new [`WhispString`] from any string type.
     pub fn new<S: Into<String>>(s: S) -> Self {
         Self(s.into())
     }
@@ -35,15 +37,56 @@ impl TryFrom<Pair<'_>> for WhispString {
 
     fn try_from(value: Pair<'_>) -> Result<Self, Self::Error> {
         match value.as_rule() {
+            // Recurse and evaluate inner node types.
             Rule::string | Rule::quoted_string_block | Rule::raw_quoted_string_block => {
                 Self::try_from(value.into_inner().next().unwrap())
             },
+            // Evaluate string directly.
             Rule::quoted_string | Rule::raw_quoted_string | Rule::unquoted_string => {
                 Ok(Self(value.as_str().to_string()))
             },
             rule => Err(Error::UnexpectedRule(rule)),
         }
     }
+}
+
+/// Function call
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FunctionCall {
+    function_name: Identifier,
+    arguments: Vec<Argument>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Identifier(String);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Argument {
+    String(WhispString),
+    StatementBlock(Box<StatementBlock>),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct StatementBlock(Vec<Statement>);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Statement {
+    LexicalDeclaration(LexicalDeclaration),
+    FunctionDeclaration(FunctionDeclaration),
+    FunctionCall(FunctionCall),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct LexicalDeclaration {
+    identifier: Identifier,
+    statement: Box<Statement>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FunctionDeclaration {
+    identifier: Identifier,
+    formal_parameters: Vec<Identifier>,
+    statement_block: StatementBlock,
 }
 
 #[cfg(test)]
